138f4855e1b9148ee6c174f36f7903b4694e7419
Aaron Bernath <abbernaths@gmail.com>
2019-04-17 17:06:26 -0700

    Update items.py

--- pdf_urls/items.py
+++ pdf_urls/items.py
@@ -7,10 +7,12 @@
 
 import scrapy
 
-
+# A class that allows you to represent the data you scrape from the web, any way you like
 class PdfUrlsItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    
+    # These are the only two things we care about scraping (according to client specifications)
     url         = scrapy.Field()
     filename    = scrapy.Field()
 
